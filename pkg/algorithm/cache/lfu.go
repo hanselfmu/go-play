91c08a7932088ca4c15e245660b56d9cad23e0ed
Changhao (Chan) Han <chan.han@shopeemobile.com>
2018-07-10 21:37:00 +0800

    finish doubly linked list for flu; pending submission pass

--- pkg/algorithm/cache/lfu.go
+++ pkg/algorithm/cache/lfu.go
@@ -9,33 +9,124 @@ type LFUCache struct {
 }
 
 type cacheValue struct {
-	val         int
-	freqLine    *freqNode
-	freqLinePos int
+	val           int
+	freqLine      *freqNode
+	keyInFreqLine *cacheKeyNode
 }
 
 type freqNode struct {
-	freq int
-	next *freqNode
-	keys []int
+	freq        int
+	next        *freqNode
+	keyListSize int
+	keyListHead *cacheKeyNode
+	keyListTail *cacheKeyNode
+}
+
+func (node *freqNode) removeHead() {
+	if node.keyListSize <= 1 {
+		node.keyListHead = nil
+		node.keyListTail = nil
+		node.keyListSize = 0
+	} else {
+		oldHead := node.keyListHead
+		node.keyListHead = oldHead.next
+		oldHead.deleteSelf()
+		node.keyListSize--
+	}
+}
+
+func (node *freqNode) appendTail(newKey *cacheKeyNode) {
+	if node.keyListSize == 0 {
+		node.keyListHead = newKey
+	} else {
+		node.keyListTail.insertAfter(newKey)
+	}
+	node.keyListTail = newKey
+	node.keyListSize++
+}
+
+type cacheKeyNode struct {
+	key  int
+	prev *cacheKeyNode
+	next *cacheKeyNode
+}
+
+func (node *cacheKeyNode) insertBefore(newNode *cacheKeyNode) {
+	if node.prev != nil {
+		node.prev.next = newNode
+		newNode.prev = node.prev
+	} else {
+		newNode.prev = nil
+	}
+	node.prev = newNode
+	newNode.next = node
+}
+
+func (node *cacheKeyNode) insertAfter(newNode *cacheKeyNode) {
+	if node.next != nil {
+		node.next.prev = newNode
+		newNode.next = node.next
+	} else {
+		newNode.next = nil
+	}
+	node.next = newNode
+	newNode.prev = node
+}
+
+func (node *cacheKeyNode) deleteSelf() {
+	if node.prev != nil {
+		node.prev.next = node.next
+	}
+	if node.next != nil {
+		node.next.prev = node.prev
+	}
+	node = nil
 }
 
 // Constructor creates a LFU cache
 func Constructor(capacity int) LFUCache {
-	freqList := freqNode{0, nil, []int{}}
+	freqList := freqNode{0, nil, 0, nil, nil}
 	return LFUCache{capacity, map[int]cacheValue{}, &freqList}
 }
 
 // Get returns a value from LFU Cache
 func (cache *LFUCache) Get(key int) int {
+	if cache.cap == 0 {
+		return -1
+	}
+
 	if res := cache.store[key]; res.freqLine != nil {
 		// move this cacheValue from its current freqNode to the next freqNode;
-		// if the next freqNode's frequency is not larger than 1, create a new freqNode
+		// if the next freqNode's frequency is exactly larger by 1, append to the next freqNode
+		// else create a new freqNode and append it there
 		curFreqNode := res.freqLine
-		curFreqNode.keys = append(curFreqNode.keys[:res.freqLinePos], curFreqNode.keys[res.freqLinePos+1:]...)
 
-		nextFreqNode := curFreqNode.next
+		if curFreqNode.keyListHead == res.keyInFreqLine {
+			curFreqNode.keyListHead = res.keyInFreqLine.next
+		}
+		if curFreqNode.keyListTail == res.keyInFreqLine {
+			curFreqNode.keyListTail = res.keyInFreqLine.prev
+		}
+		res.keyInFreqLine.deleteSelf()
+		curFreqNode.keyListSize--
+		newKeyNode := &cacheKeyNode{key, nil, nil}
 
+		if nextFreqNode := curFreqNode.next; nextFreqNode == nil {
+			// no next frequency; create a new one and append this key to its keyList
+			newFreqNode := &freqNode{curFreqNode.freq + 1, nil, 1, newKeyNode, newKeyNode}
+			curFreqNode.next = newFreqNode
+			res.freqLine = newFreqNode
+		} else if nextFreqNode.freq == curFreqNode.freq+1 {
+			res.freqLine = nextFreqNode
+			nextFreqNode.appendTail(newKeyNode)
+		} else {
+			newFreqNode := &freqNode{curFreqNode.freq + 1, nextFreqNode, 1, newKeyNode, newKeyNode}
+			curFreqNode.next = newFreqNode
+			res.freqLine = newFreqNode
+		}
+
+		res.keyInFreqLine = newKeyNode
+		cache.store[key] = res
 		return res.val
 	}
 
@@ -44,37 +135,51 @@ func (cache *LFUCache) Get(key int) int {
 
 // Put upserts a value into LFU Cache
 func (cache *LFUCache) Put(key int, value int) {
+	if cache.cap == 0 {
+		return
+	}
+
 	// checks if this key already exists; if so, updates the value
 	if cur := cache.store[key]; cur.freqLine != nil {
 		cur.val = value
+
+		// move this cacheKeyNode to the tail of its freqLine
+		newKeyNode := &cacheKeyNode{cur.keyInFreqLine.key, cur.freqLine.keyListTail, nil}
+		if cur.keyInFreqLine == cur.freqLine.keyListHead {
+			cur.freqLine.keyListHead = cur.keyInFreqLine.next
+		}
+		cur.keyInFreqLine.deleteSelf()
+		cur.keyInFreqLine = newKeyNode
+		cur.freqLine.keyListTail = newKeyNode
+		cache.store[key] = cur
 	} else {
+		freqList := cache.freqList
 		// not existing currently; evict the LFRU and create a new one
 		if len(cache.store) == cache.cap {
 			// freqList head represents 0-used values; the next freqNode
 			// represents the next smallest frequency
-			freqList := cache.freqList
-			if len(freqList.keys) > 0 {
-				evictee := freqList.keys[0]
-				delete(cache.store, evictee)
-				freqList.keys = freqList.keys[1:len(freqList.keys)]
+			if freqList.keyListSize > 0 {
+				delete(cache.store, freqList.keyListHead.key)
+				freqList.removeHead()
 			} else {
-				nextFreqKeys := freqList.next.keys
-				evictee := nextFreqKeys[0]
-				delete(cache.store, evictee)
+				curFreqNode := freqList.next
+				delete(cache.store, curFreqNode.keyListHead.key)
 
-				if len(nextFreqKeys) > 1 {
-					nextFreqKeys = nextFreqKeys[1:len(nextFreqKeys)]
+				if curFreqNode.keyListSize > 1 {
+					curFreqNode.removeHead()
 				} else {
-					oldNext := freqList.next
-					freqList.next = oldNext.next
-					oldNext = nil
+					// oldNext := freqList.next
+					// freqList.next = oldNext.next
+					// oldNext = nil
+					freqList.next = curFreqNode.next
+					curFreqNode = nil
 				}
 			}
 		}
 
-		freqList := cache.freqList
-		freqList.keys = append(freqList.keys, key)
-		cache.store[key] = cacheValue{value, freqList, len(freqList.keys) - 1}
+		newKeyNode := &cacheKeyNode{key, nil, nil}
+		freqList.appendTail(newKeyNode)
+		cache.store[key] = cacheValue{value, freqList, newKeyNode}
 	}
 }
 
--- pkg/algorithm/cache/lfu_test.go
+++ pkg/algorithm/cache/lfu_test.go
@@ -7,16 +7,34 @@ import (
 )
 
 func TestLFUCache(t *testing.T) {
-	cache := Constructor(2)
+	/*
+		["LFUCache","put","put","put","get","put","put","get","put","put","get","put","get","get","get","put","put","get","put","get"]
+		[[10],[7,28],[7,1],[8,15],[6],[10,27],[8,10],[8],[6,29],[1,9],[6],[10,7],[1],[2],[13],[8,30],[1,5],[1],[13,2],[12]]
+	*/
+	cache := Constructor(10)
+	cache.Put(7, 28)
+	cache.Put(7, 1)
+	cache.Put(8, 15)
+	assert.Equal(t, -1, cache.Get(6)) // returns -1
+	cache.Put(10, 27)
+	cache.Put(8, 10)
+	assert.Equal(t, 10, cache.Get(8))
+	cache.Put(6, 29)
+	cache.Put(1, 9)
+	assert.Equal(t, 29, cache.Get(6))
+	cache.Put(10, 7)
+	assert.Equal(t, 9, cache.Get(1))
+	assert.Equal(t, -1, cache.Get(2))
+	assert.Equal(t, -1, cache.Get(13))
+	cache.Put(8, 30)
+	cache.Put(1, 5)
+	assert.Equal(t, 5, cache.Get(1))
+	cache.Put(13, 2)
+	assert.Equal(t, -1, cache.Get(12))
 
-	cache.Put(1, 1)
-	cache.Put(2, 2)
-	assert.Equal(t, 1, cache.Get(1))  // returns 1
-	cache.Put(3, 3)                   // evicts key 2
-	assert.Equal(t, -1, cache.Get(2)) // returns -1 (not found)
-	assert.Equal(t, 3, cache.Get(3))  // returns 3.
-	cache.Put(4, 4)                   // evicts key 1.
-	assert.Equal(t, -1, cache.Get(1)) // returns -1 (not found)
-	assert.Equal(t, 3, cache.Get(3))  // returns 3
-	assert.Equal(t, 4, cache.Get(4))  // returns 4
+	/*
+		["LFUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"]
+		[[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
+	*/
+	// instructions := []string{"LFUCache", "put", "put", "put", "put", "put", "get", "put", "get", "get", "put", "get", "put", "put", "put", "get", "put", "get", "get", "get", "get", "put", "put", "get", "get", "get", "put", "put", "get", "put", "get", "put", "get", "get", "get", "put", "put", "put", "get", "put", "get", "get", "put", "put", "get", "put", "put", "put", "put", "get", "put", "put", "get", "put", "put", "get", "put", "put", "put", "put", "put", "get", "put", "put", "get", "put", "get", "get", "get", "put", "get", "get", "put", "put", "put", "put", "get", "put", "put", "put", "put", "get", "get", "get", "put", "put", "put", "get", "put", "put", "put", "get", "put", "put", "put", "get", "get", "get", "put", "put", "put", "put", "get", "put", "put", "put", "put", "put", "put", "put"}
 }
