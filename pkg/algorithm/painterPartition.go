281d538783e3bdbcd884b3ee8ec429380f7bec45
Changhao (Chan) Han <chan.han@shopeemobile.com>
2018-07-17 19:12:06 +0800

    pending painter partition

---
+++ pkg/algorithm/painterPartition.go
@@ -0,0 +1,9 @@
+package algorithm
+
+// PainterPartition solves the painter's partition problem at
+// https://www.geeksforgeeks.org/painters-partition-problem-set-2/
+func PainterPartition(k int, seq []int) []int {
+	partition := make([]int, k)
+	seqLen := len(seq)
+
+}
